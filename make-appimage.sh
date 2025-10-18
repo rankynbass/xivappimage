7e912dc74c8141418d21cd7baad11b148cc317a7
Rankyn Bass <rankyn@proton.me>
2025-10-17 23:32:16 -0700

    Extract appimage-builder to use it

--- make-appimage.sh
+++ make-appimage.sh
@@ -5,12 +5,13 @@ echo "Downloading AppImage tool"
 echo "========================="
 
 current=$(pwd)
-mkdir -p build
+mkdir -p build/output
 cd build
 if [ ! -f "appimagetool" ]; then
     wget "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage" -O "appimagetool"
     chmod +x appimagetool
 fi
+./appimagetool --appimage-extract
 update_data="gh-releases-zsync|rankynbass|xivlauncher-appimage|latest|xivlauncher-rb-x86_64.AppImage.zsync"
 appdir="${current}/xivlauncher-rb"
 output_name="xivlauncher-rb-x86_64.AppImage"
@@ -19,4 +20,5 @@ echo ""
 echo "Creating AppImage"
 echo "================="
 
-./appimagetool -u "$update_data" "$appdir" "$output_name"
+squashfs-root/AppRun -u "$update_data" "$appdir" "$output_name"
+mv xivlauncher-rb-x86_64.AppImage* output/
