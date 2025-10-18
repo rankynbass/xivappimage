cfcb6691ad08dc497ed5e17794aa5f84db54822a
Rankyn Bass <rankyn@proton.me>
2025-10-17 22:31:48 -0700

    First attempt at making appimage. Partial success

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,1 +1,13 @@
 #!/usr/bin/env bash
+current=$(pwd)
+mkdir -p build
+cd build
+if [ ! -f "appimagetool" ]; then
+    wget "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage" -O "appimagetool"
+    chmod +x appimagetool
+fi
+update_data="gh-releases-zsync|rankynbass|xivlauncher-appimage|latest|xivlauncher-rb-x86_64.AppImage.zsync"
+appdir="${current}/xivlauncher-rb"
+output_name="xivlauncher-rb-x86_64.AppImage"
+
+./appimagetool -u "$update_data" "$appdir" "$output_name"
